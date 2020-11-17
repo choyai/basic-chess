c69cda4cde2e91581230d9b6a36ea42fd0dbb2d1
Chawakorn Chaichanawirote <choyai@diluc.choyai.dev>
2020-11-18 03:10:50 +0700

    =added Chessboard class

---
+++ Chessboard.py
@@ -0,0 +1,12 @@
+
+
+class Square:
+    
+    def __init__( self,  value, color, x, y ):
+        self.value = value
+        self.color = color
+        self.x = x
+        self.y = y
+
+    def __repr__( self ):
+        return 
